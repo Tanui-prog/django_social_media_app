0ee0487fcd229a2ebbf8aaf43d9857ad18b74e28
tanui-prog <vikiikiptoo@gmail.com>
2023-12-16 00:49:35 +0300

    modified:   core/__pycache__/admin.cpython-310.pyc 	modified:   core/__pycache__/urls.cpython-310.pyc 	modified:   core/__pycache__/views.cpython-310.pyc 	modified:   core/admin.py 	modified:   core/urls.py 	modified:   core/views.py 	modified:   templates/signup.html

--- core/views.py
+++ core/views.py
@@ -5,3 +5,17 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+
+    if request.method == 'POST' :
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        
+        
+        
+        
+    else:
+        return render(request, 'signup.html')
