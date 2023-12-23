2515cbe5fda8d5924cd1d67592b4abab0f7cfa2c
tanui-prog <vikiikiptoo@gmail.com>
2023-12-24 00:27:51 +0300

    upload

--- core/views.py
+++ core/views.py
@@ -1,21 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
 from django.http import HttpResponse
+from django.contrib import messages
+
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
 
-def signup(request):
 
-    if request.method == 'POST' :
+def signup(request):
+    if request.method == 'POST':
         username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
         password2 = request.POST['password2']
-        
-        
-        
-        
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, "Email exists")
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+        else:
+            messages.info(request, 'password Not Matching')
+
+            return redirect('signup')
     else:
         return render(request, 'signup.html')
