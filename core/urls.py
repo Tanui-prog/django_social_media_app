d0fb7dfd2ab928574e9ddf7c46d5088737959739
tanui-prog <vikiikiptoo@gmail.com>
2023-12-27 23:28:39 +0300

    modified:   core/__pycache__/admin.cpython-310.pyc 	modified:   core/__pycache__/models.cpython-310.pyc 	modified:   core/__pycache__/urls.cpython-310.pyc 	modified:   core/__pycache__/views.cpython-310.pyc 	modified:   core/admin.py 	new file:   core/migrations/0002_post.py 	new file:   core/migrations/__pycache__/0002_post.cpython-310.pyc 	modified:   core/models.py 	modified:   core/urls.py 	modified:   core/views.py 	modified:   db.sqlite3 	new file:   media/post_images/IMG_2810-scaled.jpg 	new file:   media/profile_images/IMG_2810-scaled.jpg 	new file:   media/profile_images/review-1.png 	modified:   templates/index.html 	modified:   templates/setting.html

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup',views.signup,name='signup'),
+    path('upload',views.logout,name='upload'),
     path('signin',views.signin,name='signin'),
+    path('logout',views.logout,name='logout'),
+    path('settings', views.settings, name='settings'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,16 +3,67 @@ from django.contrib.auth.models import User, auth
 from django.http import HttpResponse
 from django.contrib import messages
 from .models import Profile
-
+from django.contrib.auth.decorators import  login_required
 
 
 # Create your views here.
-
+@login_required(login_url= 'signin')
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username=request.user.username) 
+
+    user_profile = Profile.objects.get(user=user_object)
+
+    return render(request, 'index.html',{'user_profile':user_profile})
+
+@login_required(login_url= 'signin')
+def upload(request):
+
+    if request.method = 'POST':
+    pass
+
+    return HttpResponse('<h2>Upload</h2>')
+
+
+@login_required(login_url= 'signin')
+def settings(request):
+    user_profile = Profile.objects.get(user = request.user)
+
+    if request.method == 'POST':
+
+        if request.FILES.get ('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location =  request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+
+            user_profile.save()
+
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            
+            user_profile.save()
+
+
+
+        return redirect('settings')
+
+
+
+
+    return render(request, 'setting.html', {'user_profile': user_profile})
 
 
 def signup(request):
+
     if request.method == 'POST':
         username = request.POST['username']
         email = request.POST['email']
@@ -32,6 +83,9 @@ def signup(request):
 
                 #log user in and redirect to settings page
 
+                user_login = auth.authenticate(username =username, password = password)
+                auth.login(request, user_login)
+
 
                 #create the profile object for new user 
 
@@ -39,12 +93,14 @@ def signup(request):
                 new_profile = Profile.objects.create(user = user_model,id_user =user_model.id)
                 new_profile.save()
         
-                return redirect('signin')
+                return redirect('settings')
 
         else:
             messages.info(request, 'Password Not Matching')
 
             return redirect('signup')
+        
+    
     else:
         return render(request, 'signup.html')
     
@@ -71,3 +127,14 @@ def signin(request):
         
 
     return render(request, 'signin.html')
+
+
+@login_required(login_url= 'signin')
+def logout (request):
+
+    auth.logout(request)
+
+    return redirect('signin')
+
+
+
