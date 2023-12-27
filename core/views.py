912a3683c359e53b5251705cb937433ee7b83fc1
tanui-prog <vikiikiptoo@gmail.com>
2023-12-27 12:09:05 +0300

    modified:   core/__pycache__/admin.cpython-310.pyc 	modified:   core/__pycache__/urls.cpython-310.pyc 	modified:   core/__pycache__/views.cpython-310.pyc 	deleted:    core/chataap.py 	modified:   core/urls.py 	modified:   core/views.py 	modified:   db.sqlite3 	modified:   templates/signin.html 	modified:   templates/signup.html

--- core/views.py
+++ core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.http import HttpResponse
 from django.contrib import messages
+from .models import Profile
+
 
 
 # Create your views here.
@@ -28,9 +30,44 @@ def signup(request):
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
 
+                #log user in and redirect to settings page
+
+
+                #create the profile object for new user 
+
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user = user_model,id_user =user_model.id)
+                new_profile.save()
+        
+                return redirect('signin')
+
         else:
-            messages.info(request, 'password Not Matching')
+            messages.info(request, 'Password Not Matching')
 
             return redirect('signup')
     else:
         return render(request, 'signup.html')
+    
+
+
+def signin(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+
+        user = auth.authenticate(username = username, password = password)
+
+
+        if user is not None:
+            auth.login (request, user)
+            return redirect('/')
+
+
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect ('signin')
+        
+
+    return render(request, 'signin.html')
